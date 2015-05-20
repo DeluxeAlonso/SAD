baea72d195dda1a6d8d5293a5c0aa18f8f9bde92
prote_000 <prote_000@192.168.1.36>
2015-05-20 01:08:07 -0500

    fase 3

--- src/infraestructure/spot/SpotRepository.java
+++ src/infraestructure/spot/SpotRepository.java
@@ -14,6 +14,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+
 import org.hibernate.criterion.Restrictions;
 
 import util.HibernateUtil;
@@ -76,6 +77,36 @@ public class SpotRepository implements ISpotRepository{
         }
         return spots; //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public List querySpotsByRackWithContent(int rackId) {
+        String hql="SELECT u.id as u_id, u.fila, u.columna, u.lado, p.id as p_id, p.ean128,p.fecha_registro "
+                + "FROM Ubicacion u,Pallet p "
+                + "WHERE u.id_rack=:rackId and p.id_ubicacion=u.id";
+        
+        List spots=null;
+        
+        Transaction trns = null;
+        Session session = Tools.getSessionInstance();
+        try {            
+            trns=session.beginTransaction();
+            Query q = session.createQuery(hql);
+            
+            //Criteria cr = session.createCriteria(Spot.class);
+            //cr.add(Restrictions.eq("id_rack", rackId));
+            q.setParameter("rackId", rackId);
+            spots = (ArrayList<Ubicacion>) q.list();      
+            //spots = cr.list(); 
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (trns != null) {
+                trns.rollback();
+            }
+            e.printStackTrace();
+        }
+        return spots; //To change body of generated methods, choose Tools | Templates.
+    }
+
     */
     @Override
     public void insert(Ubicacion object) {
