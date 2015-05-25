f042f800f0ae75c13db87946db03a2e4d50834ad
WindowsW7 <WindowsW7@MicrosoftW7>
2015-05-24 20:09:32 -0500

    Generated new SQL scripts. Major changes have been done. Please open the Workbench DB Model to check all the changes. Among all the changes, the most important ones are the removal of the 'Tipo Pallet' tables and the association of 'Orden Internamiento' with 'Item Producto' instead of 'Producto'

---
+++ sql/sql Remote BD (updated)/sqlbasedata.sql
@@ -0,0 +1,14 @@
+insert into inf226g1.Accion value (1,"Movimientos");
+insert into inf226g1.Accion value (2,"Operaciones");
+insert into inf226g1.Accion value (3,"Mantenimientos");
+insert into inf226g1.Accion value (4,"Reportes");
+insert into inf226g1.Accion value (5,"Seguridad");
+insert into inf226g1.Accion value (6,"Interfaces");
+insert into inf226g1.Accion value (7,"Sesión");
+
+insert into inf226g1.Perfil value (1,"Jefe de almacen","");
+insert into inf226g1.Perfil value (2,"Supervisor","");
+insert into inf226g1.Perfil value (3,"Administrador","");
+
+
+
