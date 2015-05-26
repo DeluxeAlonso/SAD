48d8e262277d4bdc6bd8c0c9877e4d657d1cda23
DeluxeAlonso <alonso.alvarez@pucp.pe>
2015-05-26 13:17:33 -0500

    add client column to pedidos

--- sql/sql local (updated)/sql_update_24052015.sql
+++ sql/sql local (updated)/sql_update_24052015.sql
@@ -5,6 +5,8 @@ insert into saddb.Tipo_Unidad_Transporte (id, descripcion, id_condicion, capacid
 insert into saddb.Tipo_Unidad_Transporte (id, descripcion, id_condicion, capacidad_pallets, velocidad_promedio) value (2, "Carga Mediana", 2,10,5.00);
 insert into saddb.Tipo_Unidad_Transporte (id, descripcion, id_condicion, capacidad_pallets, velocidad_promedio) value (3, "Carga Pesada", 1,12,6.00);
 
-insert into saddb.unidad_transporte value (1, "Alonso", "BGZ326", 15, 1);
-
 insert into Producto (id, nombre, descripcion, stock_total, id_condicion) value (2, "SUblime", "Sublime", 15, 1);
+
+
+ALTER TABLE pedido ADD id_cliente INT;
+ALTER TABLE pedido ADD FOREIGN KEY (id_cliente) REFERENCES Cliente(id);
