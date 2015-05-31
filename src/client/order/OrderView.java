365cbee2d5860cb17bf8cfb92720a7c12d9d1006
DeluxeAlonso <alonso.alvarez@pucp.pe>
2015-05-30 21:42:18 -0500

    order view

--- src/client/order/OrderView.java
+++ src/client/order/OrderView.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -62,16 +63,6 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
     }
 
     public void setupElements(){
-        /*ArrayList<PedidoParcial> partials = new ArrayList<>();
-        partials = orderApplication.getPendingPartialOrders();
-        for(int i=0;i<partials.size();i++){
-            System.out.println(partials.get(i).getPedido().getId() + " " + partials.get(i).getPedido().getEstado());
-            ArrayList<PedidoParcialXProducto> products = new ArrayList<>();
-            products = orderApplication.queryAllPartialOrderProducts(partials.get(i).getId());
-            for(int j=0; j<products.size(); j++){
-                System.out.println(products.get(j).getProducto().getNombre());
-            }
-        }*/
         orderApplication.refreshOrders();
         currentOrders = EntityType.ORDERS;
         fillCombos();
@@ -134,6 +125,7 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
         orderTable.addMouseListener(this);
         productTable.addMouseListener(this);
         filterClientCombo.addItemListener(this);
+        partialCombo.addItemListener(this);
     }
     
     /*ORDER METHODS*/
@@ -205,6 +197,8 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         dateTxt.setText(dateFormat.format(order.getFecha()));
         
+        partialCombo.setEnabled(true);
+        
         detailStatusCombo.setModel(new javax.swing.DefaultComboBoxModel(EntityState.getOrdersState()));
         detailStatusCombo.setSelectedIndex(order.getEstado());
         
@@ -228,7 +222,6 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
                 System.out.println(products.get(j).getProducto().getNombre());
             }
         }
-        
     }
     
     public void clearDetailFields(){
@@ -238,6 +231,7 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
         codLocalTxt.setText("");
         direccionTxt.setText("");
         dateTxt.setText("");
+        partialCombo.setEditable(false);
     }
     
     /**
@@ -287,7 +281,6 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
         detailStatusCombo = new javax.swing.JComboBox();
         jLabel13 = new javax.swing.JLabel();
         partialCombo = new javax.swing.JComboBox();
-        jButton5 = new javax.swing.JButton();
 
         setClosable(true);
         setTitle("Pedidos");
@@ -496,27 +489,27 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
 
         jLabel6.setText("Cliente:");
 
-        detailClientTxt.setEnabled(false);
+        detailClientTxt.setEditable(false);
 
         jLabel7.setText("RUC:");
 
-        rucTxt.setEnabled(false);
+        rucTxt.setEditable(false);
 
         jLabel8.setText("Cod. Local:");
 
-        codLocalTxt.setEnabled(false);
+        codLocalTxt.setEditable(false);
 
         jLabel9.setText("Direccion:");
 
         jLabel10.setText("Local:");
 
-        localTxt.setEnabled(false);
+        localTxt.setEditable(false);
 
-        direccionTxt.setEnabled(false);
+        direccionTxt.setEditable(false);
 
         jLabel11.setText("Emision:");
 
-        dateTxt.setEnabled(false);
+        dateTxt.setEditable(false);
 
         jLabel12.setText("Estado:");
 
@@ -524,12 +517,7 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
 
         jLabel13.setText("P. Parcial:");
 
-        jButton5.setText("Ver productos");
-        jButton5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
-            }
-        });
+        partialCombo.setEnabled(false);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -566,10 +554,7 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
                                         .addComponent(jLabel10)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(localTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(partialCombo, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(37, 37, 37)
-                                .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(partialCombo, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(dateTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -607,12 +592,10 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
                     .addComponent(jLabel12)
                     .addComponent(detailStatusCombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(partialCombo, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel13)
-                        .addComponent(jButton5, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(14, 14, 14))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel13)
+                    .addComponent(partialCombo))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -645,13 +628,13 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(3, 3, 3)
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
@@ -730,16 +713,6 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
         clearDetailFields();
     }//GEN-LAST:event_searchBtnActionPerformed
 
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        if(partialCombo.getSelectedIndex()==0){
-            
-        }
-        else{
-            refreshProductTable(currentPartialOrders.get(partialCombo.getSelectedIndex()-1).getId());
-        }
-        
-    }//GEN-LAST:event_jButton5ActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField codLocalTxt;
@@ -755,7 +728,6 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
@@ -823,6 +795,7 @@ public class OrderView extends javax.swing.JInternalFrame implements MouseListen
     public void itemStateChanged(ItemEvent e) {
         if (e.getStateChange() == ItemEvent.SELECTED) {
           Object item = e.getItem();
+          System.out.println(item);
           if(filterClientCombo.getSelectedIndex() != 0){
               filterLocalCombo.setEnabled(true);
               fillLocalCombo();
